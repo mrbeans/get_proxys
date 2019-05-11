4f4a16e06233bb621baaf1c1e6a7a4e6bc353173
刘永超 <liu.yongchao@synyi.com>
2019-05-11 17:47:19 +0800

    add xiladaili & requirements.txt

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 from sources import kuaidaili
+from sources import xiladaili
 import save_to_redis,test_usability
 import sys,json,shortuuid
 
+#chromedriver各版本下载地址 http://chromedriver.storage.googleapis.com/index.html
 if __name__=='__main__':
     args=sys.argv
     if(len(args)<2):
         print('请输入参数')
     elif(args[1]=='get'):
-        spider=kuaidaili.KuaiDaiLi()
+        # spider=kuaidaili.KuaiDaiLi()
+        spider=xiladaili.XiLaDaiLi()
         proxys=spider.run()
         for proxy in proxys:
             save_to_redis.save_proxy('proxys',shortuuid.uuid(),json.dumps(proxy))
--- sources/xiladaili.py
+++ sources/xiladaili.py
@@ -1,1 +1,36 @@
-#http://www.xiladaili.com/gaoni/1/
+from splinter import Browser
+import json,pdb
+
+headers={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
+    "Referer":"http://www.xiladaili.com"
+    }
+tem_url="http://www.xiladaili.com/gaoni/{0}/"
+proxy_list=[]
+
+class XiLaDaiLi(object):
+    def run(self):
+        for i in range(1,10):
+            with Browser('chrome') as browser:
+                url=tem_url.format(i)
+                browser.visit(url)
+
+                tr_list=browser.find_by_tag('tbody').first.find_by_tag('tr')
+                if(tr_list==None or len(tr_list)<=0):
+                    print('page {0} get 0 proxy,return'.format(i))
+                    return
+                print('page {0} get {1} proxys'.format(i,len(tr_list)))
+                for tr in tr_list:
+                    ip_port=tr.find_by_tag('td')[0].value
+                    ip=ip_port.split(':')[0]
+                    port=ip_port.split(':')[1]
+                    protocal_value=tr.find_by_tag('td')[1].value
+                    if('HTTP' in protocal_value):
+                        protocal='HTTP'
+                    if('HTTPS' in protocal_value):
+                        protocal='HTTPS'
+                    result={'ip':ip,'port':port,'protocal':protocal}
+                    proxy_list.append(result)
+                    print('get new proxy : '+json.dumps(result))
+            print('page {0} process down!'.format(i))
+        return proxy_list
