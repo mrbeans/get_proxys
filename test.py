664a376c565f44983adb018e7a7d46afad143f51
刘永超 <liuyongchao@liuyongchaodeiMac.local>
2019-08-03 21:30:07 +0800

    windows和mac风格不同

--- test.py
+++ test.py
@@ -1,7 +1,25 @@
+from tesserocr import PyTessBaseAPI
 
-import tesserocr
+images = ['1-26.jpg','Unknown.png','8080.gif']
+
+# with PyTessBaseAPI() as api:
+#     for img in images:
+#         api.SetImageFile(img)
+#         print(api.GetUTF8Text())
+
+import sys
+from pytesseract import *
+import requests
+import os
+import re
 from PIL import Image
+from PIL import ImageEnhance
+import pkg_resources
 
-image = Image.open(r'img/logo.png')   #传入你所保存的图片路径
-result = tesserocr.image_to_text(image)
-print(result)
+image = Image.open('8080.gif')
+image = image.convert('L')
+threshold = 250 
+initTable = lambda x:0 if x < threshold else 1
+binaryImage = image.point(initTable, '1')
+vcode = image_to_string(binaryImage, lang="eng", config='-psm 7')
+print vcode.encode('utf-8').replace(' ','')
